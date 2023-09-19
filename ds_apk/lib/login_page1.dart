9b2cda5f9ab6ac660cb14af3bb6f3850b2806147
almosrof120 <mosrof35-645@diu.edu.bd>
2023-09-19 22:44:29 +0600

    svg updated

--- ds_apk/lib/login_page1.dart
+++ ds_apk/lib/login_page1.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key, required this.title}) : super(key: key);
@@ -16,9 +17,29 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+
+
    
     return Scaffold(
+      
       backgroundColor: Color.fromARGB(255, 32, 72, 149),
+      body: Column(
+        children: [
+          SvgPicture.asset('Images/svg/Vector 10.svg',
+          height: 240,
+          width: 100,
+          matchTextDirection: true,
+
+          ),
+          
+        ],
+      ),
+
+
+      
+      
     );
+
+    
   }
 }
