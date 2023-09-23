c3a96afc2f008961b504f10481a6565a022bf070
almosrof120 <mosrof35-645@diu.edu.bd>
2023-09-24 04:14:08 +0600

    custom design

--- ds_apk/lib/login_page1.dart
+++ ds_apk/lib/login_page1.dart
@@ -1,86 +1,24 @@
-import 'dart:html';
-
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-import 'package:ds_apk/home.dart';
-import 'package:ds_apk/myflutter_app_icons.dart';
-import 'package:ds_apk/profile.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+
+
+
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
+  const HomePage({super.key, required String title});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
- final items = const [
-    Icon(Icons.home, size: 25,),
-    Icon(Icons.event, size: 25,),
-    Icon(Icons.menu, size: 25,),
-    Icon(Icons.groups_sharp, size: 25,),
-    Icon(Icons.person_2, size: 25,)
-
-  ];
-
-  int index = 1;
-
- 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
 
-    return  Scaffold(
-//       appBar: PreferredSize(preferredSize:const Size.fromHeight(50), child: AppBar(
-//                backgroundColor:const Color.fromARGB(255, 255, 255, 255),
-//   actions: [
-//         Image.asset("Images/ds.png",
-//         height: 100,
-//         width: 100,
-//         ),
-//         SizedBox(
-//             width: MediaQuery.of(context).size.width / 3,
-//           ),
-//           Image.asset(
-//             "Images/diu.png",
-//             height: 100,
-//             width: 100,
-//           ),
-//           InkWell(
-//             onTap: () {},
-//             child: Image.asset(
-//               "Images/notification.gif",
-//               scale: 2,
-//             ),
-//           )
-//         ],
-//       ),
-//  ),
-// backgroundColor:const Color.fromARGB(255, 32, 72, 149),
-// body: Center(child:
-//             Column(
-//         children: [
-//           SvgPicture.asset('Images/svg/Vector 10.svg',
-//           height: 240,
-//           width: 100,
-//           matchTextDirection: true,
-//           ),
-//         ],
-//       ),
-//            ),
-
-
-//class _HomeState extends State<Home> {
-    
-  
-
-
-  
-      backgroundColor: const Color.fromARGB(255, 32, 72, 149),
       appBar: PreferredSize(preferredSize:const Size.fromHeight(50), child: AppBar(
+        elevation: 0,
                backgroundColor:const Color.fromARGB(255, 255, 255, 255),
-  actions: [
+       actions: [
         Image.asset("Images/ds.png",
         height: 100,
         width: 100,
@@ -101,52 +39,53 @@ class _HomePageState extends State<HomePage> {
             ),
           )
         ],
+      )
       ),
- ),
-
-body: Center(child:
-            Column(
-        children: [
-          SvgPicture.asset('Images/svg/Vector 10.svg',
-          height: 240,
-          width: 100,
-          matchTextDirection: true,
-          ),
-        ],
-      ),
-           ),
-
-
-
-
-
-      
-      bottomNavigationBar: CurvedNavigationBar(
-
-
-
-        buttonBackgroundColor:const Color.fromARGB(255, 32, 72, 149),
-        
-
-
-
-
-
-        items: items,
-        index: index,
-        onTap: (selctedIndex){
-          setState(() {
-            index = selctedIndex;
-          });
-        },
-        height: 60,
-        backgroundColor: const Color.fromARGB(255, 32, 72, 149),
-        animationDuration: const Duration(milliseconds: 300),
-        color: Color.fromARGB(255, 66, 115, 206),
-        // animationCurve: ,
-      ),
-      
-      );
     
+      body:
+         Column(
+          children: [
+        
+            ClipPath(
+              clipper: Customclippath(),
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                color: const Color.fromARGB(255, 32, 72, 149),
+        
+                
+              ),
+            ),
+          ],
+        ),
+    );
+  }
+  }
+
+
+class Customclippath extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+   
+
+    
+         
+    Path path = Path();
+    path.moveTo(size.width*-0.0010000,size.height*0.1362857);
+    path.lineTo(size.width*0.0008333,size.height*1.0042857);
+    path.lineTo(size.width*0.9991667,size.height*0.9971429);
+    path.lineTo(size.width*0.9991667,size.height*0.0028571);
+    path.lineTo(size.width*0.9995000,size.height*0.1068571);
+    path.lineTo(size.width*0.1435000,size.height*0.2154286);
+
+
+  
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
